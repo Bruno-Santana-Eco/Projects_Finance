59989e085059030a64a7859ed691ddf4b441f028
Bruno Santana <brunosantana.eco@gmail.com>
2023-12-16 22:49:26 -0300

    1

---
+++ CAPM e FAMA.R
@@ -0,0 +1,15 @@
+install.packages("devtools")
+devtools::install_github("Reckziegel/nefin")
+
+library(nefin)
+
+risk_factors()
+
+
+dividend_yield()
+risk_free()
+rm_minus_rf()
+cost_of_equity()
+
+install.packages('devtools') # uncomment if devtools needs to be installed
+devtools::install_github('fernandoramacciotti/nefindata/R')
